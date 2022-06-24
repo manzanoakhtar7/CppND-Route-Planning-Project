260bd887c4c6d0450ac531da804e1ed6e9a38b01
Manzano Akhtar <manzano.akhtar@gmail.com>
2022-06-24 11:55:08 -0400

    complete TODO 1, allow user input of start and end position, add .vscode to .gitignore

--- src/main.cpp
+++ src/main.cpp
@@ -55,12 +55,24 @@ int main(int argc, const char **argv)
     // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below in place of 10, 10, 90, 90.
+    float start_x, start_y, end_x, end_y;
+    std::cout << "Enter a starting x position (between 0 and 100): ";
+    std::cin >> start_x;
+    std::cout << "Enter a starting y position (between 0 and 100): ";
+    std::cin >> start_y;
+    std::cout << "Enter a ending x position (between 0 and 100): ";
+    std::cin >> end_x;
+    std::cout << "Enter a ending y position (between 0 and 100): ";
+    std::cin >> end_y;
 
+    // Write the input positions back to the user
+    std::cout << "Starting Position: " << start_x <<"," << start_y << std::endl;
+    std::cout << "Ending Position: " << end_x <<"," << end_y << std::endl;
     // Build Model.
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
